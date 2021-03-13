991d46f301b9ad7bde5e535e252f7c2f94dd4c98
Brian Clovis <bwclovis.web@gmail.com>
2021-03-13 08:05:36 -0500

    update global config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `a11y acceptabce criteria`,
+    description: `Accessibility aceptance criteria and information for componentnts`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
